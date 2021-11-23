b82ef7e855d7de8e8ed6daf2c6ffb5c45d134cd6
ariniindah <94271353+ariniindah@users.noreply.github.com>
2021-11-23 10:17:16 +0700

    Delete ETL/ETL directory

--- ETL/ETL/query_2.sql
+++
@@ -1,9 +0,0 @@
-SET @user := 0;
-SELECT @group := id_user FROM purchasehistory WHERE id_user = @user;
-SELECT @GROUP
-
-SET @user := 0;
-SELECT @sumtrxamt := SUM(transactionAmount)
- 	FROM purchasehistory
-	WHERE id_user = @user;
-SELECT @sumtrxamt
